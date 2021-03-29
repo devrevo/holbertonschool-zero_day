40c178e35c0ffb4ec73b6224b9843c0d6e774709
devrevo <ayoub.mafkoud@gmail.com>
2021-03-29 15:11:38 +0000

    TASK 1: EC_KEY creation

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,5 +1,12 @@
+
 #pragma once
 #include <stdint.h>
 #include <openssl/sha.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+
+# define EC_CURVE   NID_secp256k1
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+
+EC_KEY *ec_create(void);
