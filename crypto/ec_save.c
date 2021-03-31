e11ec45e41e2f9c4d419514a3d8beaa26eae8400
devrevo <ayoub.mafkoud@gmail.com>
2021-03-31 17:45:38 +0000

    Task 4. EC_KEY - Save to file

---
+++ crypto/ec_save.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <openssl/pem.h>
+
+#include "hblk_crypto.h"
+
+int ec_save(EC_KEY *key, char const *folder){
+  
+  char path[BUFSIZ];
+  FILE *fp;
+
+  if(!EC_KEY_check_key(key) || !key || !folder || strlen(folder) + strlen(PUB_FILENAME) > BUFSIZ)
+    return 0;
+  
+  mkdir(folder, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+  sprintf(path, "%s/%s", folder, PUB_FILENAME);
+  fp = fopen(path,"w");
+  if(!fp)
+    return 0;
+  if(!PEM_write_EC_PUBKEY(fp,key))
+    goto clean;
+  fclose(fp);
+  sprintf(path, "%s/%s", folder, PRI_FILENAME);
+  fp = fopen(path,"w");
+  if(!fp)
+    return 0;
+  if(!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
+    goto clean;
+  fclose(fp);
+  return 1;
+ clean:
+  fclose(fp);
+  return 0;
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,11 +1,15 @@
 
 #pragma once
 #include <stdint.h>
+#include <string.h>
 #include <openssl/sha.h>
 #include <openssl/ec.h>
+#include <sys/stat.h>
 #include <openssl/obj_mac.h>
 
 # define EC_CURVE   NID_secp256k1
+# define PRI_FILENAME   "key.pem"
+# define PUB_FILENAME   "key_pub.pem"
 # define EC_PUB_LEN 65
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
@@ -15,3 +19,5 @@ EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+
+int ec_save(EC_KEY *key, char const *folder);
